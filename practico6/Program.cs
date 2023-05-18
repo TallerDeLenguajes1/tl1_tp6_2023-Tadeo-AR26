79629aa39f55880692a6ee361fb568ad4472f3e7
Tadeo-AR26 <alonsoruiztadeo26@gmail.com>
2023-05-18 17:59:32 -0300

    Carpeta Practico06

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,14 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
 int a;
-
 int b;
-
 a = 10;
-
 b = a;
-
-Console.WriteLine("valor de a:" +a);
-
-Console.WriteLine("valor de b:" +b);
+Console.WriteLine("El valor de a:"+a);
+Console.WriteLine("El valor de b:"+b);
