5745f2df84fe785eaf53a9526a98954d2fdd22ec
Tadeo-AR26 <alonsoruiztadeo26@gmail.com>
2023-05-18 18:40:44 -0300

    Ejercicio 01

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,8 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Hello, World!");
 int a;
 int b;
 a = 10;
 b = a;
 Console.WriteLine("El valor de a:"+a);
 Console.WriteLine("El valor de b:"+b);
+*/
+
+// Ejercicio01
+int a = 0;
+bool esNumero = false;
+string? StringNumero = "";
+string? NumeroInvertido = "";
+
+while(!esNumero){
+    Console.WriteLine("Escriba un numero");
+    StringNumero = Console.ReadLine();
+    esNumero = int.TryParse(StringNumero, out a);
+    if(!esNumero){
+        Console.WriteLine("No se ha ingresado un numero valido");
+    }
+    else{
+        for(int i = StringNumero.Length-1; i>=0; i--){
+            NumeroInvertido += StringNumero[i];
+        }
+    }
+}
+
+Console.WriteLine("El numero invertido es: "+NumeroInvertido);
+
